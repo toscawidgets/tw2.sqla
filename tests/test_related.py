a9abff93268e7394d617d938b52e218643e9f03d
Ralph Bean <ralph.bean@gmail.com>
2010-11-23 14:24:35 -0500

    Consolidated related_tests

--- tests/test_related.py
+++ tests/test_related.py
@@ -1,123 +1,109 @@
 import tw2.core as twc, tw2.sqla as tws, sqlalchemy as sa
+import testapi
 
-#-----------------------
-# Elixir
-#-----------------------
-import elixir as el
-el.metadata = sa.MetaData('sqlite:///:memory:')
+class BaseObject(object):
+    def setUp(self):
+        raise NotImplementedError, "Must be subclassed."
 
-class ElTest(el.Entity):
-    name = el.Field(el.String)
-class ElTest2(el.Entity):
-    id = el.Field(el.String, primary_key=True)
-class ElTest3(el.Entity):
-    id1 = el.Field(el.Integer, primary_key=True)
-    id2 = el.Field(el.Integer, primary_key=True)
-el.setup_all()
-el.metadata.create_all()
+    def test_null(self):
+        vld = tws.RelatedValidator(self.DBTestCls1)
+        assert(vld.from_python(None) is None)
+        assert(vld.to_python('') is None)
 
-ElTest(id=1)
-ElTest2(id='bob')
-el.session.commit()
+    def test_int(self):
+        vld = tws.RelatedValidator(self.DBTestCls1)
+        t1 = vld.to_python('1')    
+        assert(isinstance(t1, self.DBTestCls1))
+        assert(t1.id == 1)
+        assert(vld.from_python(t1) == u'1')
+        try:
+            vld.to_python('x')
+            assert(False)
+        except twc.ValidationError:
+            pass
+        try:
+            vld.to_python('2')
+            assert(False)
+        except twc.ValidationError:
+            pass
+
+    def test_nonint(self):
+        vld = tws.RelatedValidator(self.DBTestCls2)
+        t1 = vld.to_python('bob')
+        assert(isinstance(t1, self.DBTestCls2))
+        assert(t1.id == 'bob')
+        assert(vld.from_python(t1) == u'bob')
+        try:
+            vld.to_python('x')
+            assert(False)
+        except twc.ValidationError:
+            pass
+
+    def test_twopk_dec(self):
+        try:
+            tws.RelatedValidator(self.DBTestCls3)
+            assert(False)
+        except twc.WidgetError, e:
+            assert(str(e) == 'RelatedValidator can only act on tables that have a single primary key column')
 
 
-def test_int_el():
-    vld = tws.RelatedValidator(ElTest)
-    assert(vld.to_python('1') is ElTest.get(1))
-    assert(vld.from_python(ElTest.get(1)) == u'1')
-    try:
-        vld.to_python('x')
-        assert(False)
-    except twc.ValidationError:
-        pass
-    try:
-        vld.to_python('2')
-        assert(False)
-    except twc.ValidationError:
-        pass
+
+class TestElixir(BaseObject):
+    def setUp(self):
+        import elixir as el
+        import transaction
+        el.metadata = sa.MetaData('sqlite:///:memory:')
+
+        class DBTestCls1(el.Entity):
+            name = el.Field(el.String)
+        class DBTestCls2(el.Entity):
+            id = el.Field(el.String, primary_key=True)
+        class DBTestCls3(el.Entity):
+            id1 = el.Field(el.Integer, primary_key=True)
+            id2 = el.Field(el.Integer, primary_key=True)
         
-def test_nonint_el():
-    vld = tws.RelatedValidator(ElTest2)
-    assert(vld.to_python('bob') is ElTest2.get('bob'))
-    assert(vld.from_python(ElTest2.get('bob')) == u'bob')
-    try:
-        vld.to_python('x')
-        assert(False)
-    except twc.ValidationError:
-        pass
+        self.DBTestCls1 = DBTestCls1
+        self.DBTestCls2 = DBTestCls2
+        self.DBTestCls3 = DBTestCls3
+    
+        el.setup_all()
+        el.metadata.create_all()
 
-def test_twopk_el():
-    try:
-        tws.RelatedValidator(ElTest3)
-        assert(False)
-    except twc.WidgetError, e:
-        assert(str(e) == 'RelatedValidator can only act on tables that have a single primary key column')
+        self.DBTestCls1(id=1)
+        self.DBTestCls2(id='bob')
+        transaction.commit()
 
-def test_null():
-    vld = tws.RelatedValidator(ElTest)
-    assert(vld.from_python(None) is None)
-    assert(vld.to_python('') is None)
+        testapi.setup()
 
+class TestSQLA(BaseObject):
+    def setUp(self):
+        from sqlalchemy.ext.declarative import declarative_base
+        Base = declarative_base(metadata=sa.MetaData('sqlite:///:memory:'))
+        Base.query = tws.transactional_session().query_property()
 
-#-----------------------
-# SQLAlchemy declarative
-#-----------------------
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base(metadata=sa.MetaData('sqlite:///:memory:'))
-Base.query = tws.transactional_session().query_property()
+        class DBTestCls1(Base):
+            __tablename__ = 'Test'
+            id = sa.Column(sa.Integer, primary_key=True)
+            name = sa.Column(sa.String(50))
+        class DBTestCls2(Base):
+            __tablename__ = 'Test2'
+            id = sa.Column(sa.String, primary_key=True)
+        class DBTestCls3(Base):
+            __tablename__ = 'Test3'
+            id1 = sa.Column(sa.Integer, primary_key=True)
+            id2 = sa.Column(sa.Integer, primary_key=True)
 
-class DecTest(Base):
-    __tablename__ = 'Test'
-    id = sa.Column(sa.Integer, primary_key=True)
-    name = sa.Column(sa.String(50))
-class DecTest2(Base):
-    __tablename__ = 'Test2'
-    id = sa.Column(sa.String, primary_key=True)
-class DecTest3(Base):
-    __tablename__ = 'Test3'
-    id1 = sa.Column(sa.Integer, primary_key=True)
-    id2 = sa.Column(sa.Integer, primary_key=True)
+        self.DBTestCls1 = DBTestCls1
+        self.DBTestCls2 = DBTestCls2
+        self.DBTestCls3 = DBTestCls3
+    
+        Base.metadata.create_all()
 
-Base.metadata.create_all()
+        session = sa.orm.sessionmaker()()
+        self.session = session
+        session.add(self.DBTestCls1(id=1))
+        session.add(self.DBTestCls2(id='bob'))
+        session.commit()
 
-session = sa.orm.sessionmaker()()
-session.add(DecTest(id=1))
-session.add(DecTest2(id='bob'))
-session.commit()
+        testapi.setup()
 
-
-def test_int_dec():
-    vld = tws.RelatedValidator(DecTest)
-    t1 = vld.to_python('1')    
-    assert(isinstance(t1, DecTest))
-    assert(t1.id == 1)
-    assert(vld.from_python(t1) == u'1')
-    try:
-        vld.to_python('x')
-        assert(False)
-    except twc.ValidationError:
-        pass
-    try:
-        vld.to_python('2')
-        assert(False)
-    except twc.ValidationError:
-        pass
-        
-def test_nonint_dec():
-    vld = tws.RelatedValidator(DecTest2)
-    t1 = vld.to_python('bob')
-    assert(isinstance(t1, DecTest2))
-    assert(t1.id == 'bob')
-    assert(vld.from_python(t1) == u'bob')
-    try:
-        vld.to_python('x')
-        assert(False)
-    except twc.ValidationError:
-        pass
-
-def test_twopk_dec():
-    try:
-        tws.RelatedValidator(DecTest3)
-        assert(False)
-    except twc.WidgetError, e:
-        assert(str(e) == 'RelatedValidator can only act on tables that have a single primary key column')
---
+++ tests/testapi.py
@@ -0,0 +1,33 @@
+""" Copied from tw2.core """
+
+import tw2.core as twc
+from tw2.core.template import reset_engine_name_cache
+
+def request_local_tst():
+#    if _request_id is None:
+#        raise KeyError('must be in a request')
+    try:
+        return _request_local[_request_id]
+    except KeyError:
+        rl_data = {}
+        _request_local[_request_id] = rl_data
+        return rl_data
+
+twc.core.request_local = request_local_tst
+_request_local = {}
+
+def setup():
+    global _request_local, _request_id
+    _request_local = {}
+    _request_id = None
+    reset_engine_name_cache()
+
+
+def request(requestid, mw=None):
+    global _request_id
+    _request_id = requestid
+    rl = twc.core.request_local()
+    rl.clear()
+    rl['middleware'] = mw
+    return request_local_tst()
+
