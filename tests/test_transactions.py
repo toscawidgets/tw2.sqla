b6093ed24cdfe32e6eb763c9b86e15b40d886792
Ralph Bean <ralph.bean@gmail.com>
2010-11-27 00:02:29 -0500

    Moving tests into classes.

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -1,58 +1,67 @@
 import tw2.core as twc, tw2.sqla as tws, sqlalchemy as sa, elixir as el, webtest, webob as wo, threading, os
 
+#import testapi
 
-# Note: we can't use :memory: from two threads
-if os.path.exists('test.db'):
-    os.unlink('test.db')
-el.metadata = sa.MetaData('sqlite:///test.db')
-el.session = tws.transactional_session()
-class Test(el.Entity):
-    name = el.Field(el.String)
-el.setup_all()
-el.metadata.create_all()
+class TestTransactions(object):
+    def setup(self):
+        #testapi.setup()
+        # Note: we can't use :memory: from two threads
+        if os.path.exists('test.db'):
+            os.unlink('test.db')
+        el.metadata = sa.MetaData('sqlite:///test.db')
+        el.session = tws.transactional_session()
+        class Test(el.Entity):
+            name = el.Field(el.String)
+        self.DBEnt = Test
+        el.setup_all()
+        el.metadata.create_all()
 
+        self.DBEnt(name='rjb')
+        import transaction
+        transaction.commit()
 
-def app(environ, start_response):
-    req = wo.Request(environ)
-    resp = wo.Response(request=req, content_type="text/html; charset=UTF8")
-    if req.path == '/createok':
-        Test(name='paj')
-    if req.path == '/createfail':
-        Test(name='paj')
-        resp.status = 404
-    if req.path == '/read':
-        global keep # ensure the object isn't expired due to weakref session
-        keep = Test.query.all()[0]
-        resp.body = keep.name.encode('utf-8')
-    if req.path == '/update': # Change object
-        Test.query.all()[0].name = 'bob'
-    return resp(environ, start_response)
-app = twc.make_middleware(app, repoze_tm=True)
-app = webtest.TestApp(app)
+        def app(environ, start_response):
+            req = wo.Request(environ)
+            resp = wo.Response(
+                request=req, content_type="text/html; charset=UTF8")
+            if req.path == '/createok':
+                self.DBEnt(name='paj')
+            if req.path == '/createfail':
+                self.DBEnt(name='paj')
+                resp.status = 404
+            if req.path == '/read':
+                # ensure the object isn't expired due to weakref session
+                global keep
+                keep = self.DBEnt.query.all()[0]
+                resp.body = keep.name.encode('utf-8')
+            if req.path == '/update': # Change object
+                self.DBEnt.query.all()[0].name = 'bob'
+            return resp(environ, start_response)
+        self.app = twc.make_middleware(app, repoze_tm=True)
+        self.app = webtest.TestApp(self.app)
 
+    def test_createok(self):
+        el.session.remove()
+        before = len(self.DBEnt.query.all())
+        self.app.get('/createok')
+        el.session.remove()
+        assert(len(self.DBEnt.query.all()) == before + 1)
 
-def test_createok():
-    el.session.remove()
-    before = len(Test.query.all())
-    app.get('/createok')
-    el.session.remove()
-    assert(len(Test.query.all()) == before + 1)
+    def test_createfail(self):
+        el.session.remove()
+        before = len(self.DBEnt.query.all())
+        self.app.get('/createfail', expect_errors=True)
+        el.session.remove()
+        assert(len(self.DBEnt.query.all()) == before)
 
-def test_createfail():
-    el.session.remove()
-    before = len(Test.query.all())
-    app.get('/createfail', expect_errors=True)
-    el.session.remove()
-    assert(len(Test.query.all()) == before)
-
-def test_freshness():
-    assert(app.get('/read').body == 'paj')
-    # Update the object in a separate thread
-    class ThreadB(threading.Thread):
-        def run(self):
-            app.get('/update')
-    thrdb = ThreadB()
-    thrdb.start()
-    thrdb.join()
-    # Check it's updated in the first thread
-    assert(app.get('/read').body == 'bob')
+    def test_freshness(self):
+        assert(self.app.get('/read').body == 'rjb')
+        # Update the object in a separate thread
+        class ThreadB(threading.Thread):
+            def run(shmelf):
+                self.app.get('/update')
+        thrdb = ThreadB()
+        thrdb.start()
+        thrdb.join()
+        # Check it's updated in the first thread
+        assert(self.app.get('/read').body == 'bob')
---
+++ tests/test_widgets.py
@@ -0,0 +1,103 @@
+import tw2.core as twc, tw2.sqla as tws, sqlalchemy as sa
+import tw2.core.testbase as tw2test
+
+import testapi
+
+
+class ElixirBase(object):
+    def setup(self):
+        import elixir as el
+        import transaction
+        el.metadata = sa.MetaData('sqlite:///:memory:')
+
+        class DBTestCls1(el.Entity):
+            name = el.Field(el.String)
+            def __unicode__(self):
+                return self.name
+        class DBTestCls2(el.Entity):
+            id = el.Field(el.String, primary_key=True)
+        class DBTestCls3(el.Entity):
+            id1 = el.Field(el.Integer, primary_key=True)
+            id2 = el.Field(el.Integer, primary_key=True)
+        
+        self.DBTestCls1 = DBTestCls1
+        self.DBTestCls2 = DBTestCls2
+        self.DBTestCls3 = DBTestCls3
+    
+        el.setup_all()
+        el.metadata.create_all()
+
+        self.DBTestCls1(id=1, name='foo1')
+        self.DBTestCls1(id=2, name='foo2')
+        self.DBTestCls2(id='bob')
+        transaction.commit()
+
+        super(ElixirBase, self).setup()
+        testapi.request(1)
+
+class SQLABase(object):
+    def setup(self):
+        from sqlalchemy.ext.declarative import declarative_base
+        Base = declarative_base(metadata=sa.MetaData('sqlite:///:memory:'))
+        Base.query = tws.transactional_session().query_property()
+
+        class DBTestCls1(Base):
+            __tablename__ = 'Test'
+            id = sa.Column(sa.Integer, primary_key=True)
+            name = sa.Column(sa.String(50))
+            def __unicode__(self):
+                return self.name
+        class DBTestCls2(Base):
+            __tablename__ = 'Test2'
+            id = sa.Column(sa.String, primary_key=True)
+            other_id = sa.Column(sa.Integer, sa.ForeignKey('Test.id'))
+            other = sa.orm.relation(DBTestCls1, backref=sa.orm.backref('others'))
+        class DBTestCls3(Base):
+            __tablename__ = 'Test3'
+            id1 = sa.Column(sa.Integer, primary_key=True)
+            id2 = sa.Column(sa.Integer, primary_key=True)
+
+        self.DBTestCls1 = DBTestCls1
+        self.DBTestCls2 = DBTestCls2
+        self.DBTestCls3 = DBTestCls3
+    
+        Base.metadata.create_all()
+
+        session = sa.orm.sessionmaker()()
+        self.session = session
+        session.add(self.DBTestCls1(id=1, name='foo1'))
+        session.add(self.DBTestCls1(id=2, name='foo2'))
+        session.add(self.DBTestCls2(id='bob'))
+        session.commit()
+
+        super(SQLABase, self).setup()
+        testapi.request(1)
+
+class RadioButtonT(tw2test.WidgetTest):
+    widget = tws.DbCheckBoxList
+    attrs = {'css_class':'something', 'id' : 'something'}
+    params = {'checked':None}
+    expected = """
+    <ul class="something" name="something" id="something">
+    <li>
+        <input type="checkbox" name="something" value="1" id="something:0"/>
+        <label for="something:0">foo1</label>
+    </li>
+    <li>
+        <input type="checkbox" name="something" value="2" id="something:1"/>
+        <label for="something:1">foo2</label>
+    </li>
+    </ul>"""
+
+    
+    def setup(self):
+        self.params['entity'] = self.DBTestCls1
+        return super(RadioButtonT, self).setup()
+
+# TODO -- how to share the testbase attrs between these...
+#  Note.. I think that tw2test.WidgetTest never calls super(..).setUp()
+#  :(
+class TestRadioButtonElixir(ElixirBase, RadioButtonT):
+    pass
+class TestRadioButtonSQLA(SQLABase, RadioButtonT):
+    pass
