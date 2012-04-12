fcc1d1156a48137c5f691298f5959231e7c2f3f5
Ralph Bean <rbean@redhat.com>
2012-04-11 20:19:00 -0400

    Fixed odd regression in tests.  Relates to #1.

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -991,7 +991,7 @@ class AutoGrowingGridT(tw2test.WidgetTest):
     <table id="autogrid">
         <tr>
             <th>Name</th><th>Others</th><th></th>
-            <td><input style="display:none" type="image" id="autogrid:undo" src="/resources/tw2.dynforms.widgets/static/undo.png" alt="Undo" onclick="twd_grow_undo(this); return false;"></td>
+            <td><input style="display:none" type="image" id="autogrid:undo" src="/resources/tw2.dynforms.widgets/static/undo.png" alt="Undo" title="Undo" onclick="twd_grow_undo(this); return false;"></td>
         </tr>
         <tr style="display:none;" id="autogrid:0" class="odd">
         <td>
@@ -1039,7 +1039,7 @@ class AutoGrowingGridAsChildT(tw2test.WidgetTest):
     <table id="autogrid">
         <tr>
             <th>Name</th><th>Others</th><th></th>
-            <td><input style="display:none" type="image" id="autogrid:undo" src="/resources/tw2.dynforms.widgets/static/undo.png" alt="Undo" onclick="twd_grow_undo(this); return false;"></td>
+            <td><input style="display:none" type="image" id="autogrid:undo" src="/resources/tw2.dynforms.widgets/static/undo.png" alt="Undo" title="Undo" onclick="twd_grow_undo(this); return false;"></td>
         </tr>
         <tr style="display:none;" id="autogrid:0" class="odd">
         <td>
@@ -1087,7 +1087,7 @@ class AutoGrowingGridAsChildWithRelationshipT(tw2test.WidgetTest):
     <table id="others">
         <tr>
             <th>Nick</th><th>Other</th><th></th>
-            <td><input style="display:none" type="image" id="others:undo" src="/resources/tw2.dynforms.widgets/static/undo.png" alt="Undo" onclick="twd_grow_undo(this); return false;"></td>
+            <td><input style="display:none" type="image" id="others:undo" src="/resources/tw2.dynforms.widgets/static/undo.png" alt="Undo" title="Undo" onclick="twd_grow_undo(this); return false;"></td>
         </tr>
         <tr style="display:none;" id="others:0" class="odd">
         <td>
