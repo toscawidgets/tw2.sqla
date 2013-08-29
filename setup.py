382a27e607be5e4812b6e025b36ed465ededae10
Moritz Schlarb <moschlar@metalabs.de>
2013-08-29 11:09:13 +0200

    Metadata fixups, tw2.core#90

--- setup.py
+++ setup.py
@@ -23,11 +23,12 @@ requires = [
 setup(
     name='tw2.sqla',
     version='2.0.6',
-    description='SQLAlchemy database layer for ToscaWidgets 2',
+    description='SQLAlchemy database layer for ToscaWidgets 2, a web widget toolkit.',
     long_description=open('README.rst').read().split('.. split here', 1)[1],
     author='Paul Johnston',
     author_email='paj@pajhome.org.uk',
     url='http://github.com/toscawidgets/tw2.sqla',
+    download_url="https://pypi.python.org/pypi/tw2.sqla/",
     license='MIT',
     install_requires=requires,
     packages=find_packages(exclude=['ez_setup', 'tests']),
